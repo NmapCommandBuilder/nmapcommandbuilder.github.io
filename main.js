64e6c77a8d55def9f427e3d604069031bee286da
nmapcommandbuilder <nmapcommandbuilder@gmail.com>
2023-05-05 20:31:35 +0800

    reset code

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function showContent(content) {
-	// Get all tab links and content sections
-	var tablinks = document.getElementsByClassName('tablinks');
-	var sections = document.getElementsByClassName('content-section');
+  // Get all tab links and content sections
+  var tablinks = document.getElementsByClassName('tablinks');
+  var sections = document.getElementsByClassName('content-section');
 
-	// Remove the active class from all tab links and content sections
-	for (var i = 0; i < tablinks.length; i++) {
-		tablinks[i].classList.remove('active');
-		sections[i].classList.remove('active');
-	}
+  // Remove the active class from all tab links and content sections
+  for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove('active');
+    sections[i].classList.remove('active');
+  }
 
-	// Add the active class to the selected tab link and content section
-	document.getElementById(content + '-tab').classList.add('active');
-	document.getElementById(content + '-section').classList.add('active');
+  // Add the active class to the selected tab link and content section
+  document.getElementById(content + '-tab').classList.add('active');
+  document.getElementById(content + '-section').classList.add('active');
 }
