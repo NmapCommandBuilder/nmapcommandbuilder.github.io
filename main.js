57538eafee9586fdda953a415652686b64e59e06
nmapcommandbuilder <nmapcommandbuilder@gmail.com>
2023-05-05 20:19:16 +0800

    a

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 function showContent(content) {
-	// Get all content sections
+	// Get all tab links and content sections
+	var tablinks = document.getElementsByClassName('tablinks');
 	var sections = document.getElementsByClassName('content-section');
 
-	// Hide all content sections
-	for (var i = 0; i < sections.length; i++) {
+	// Remove the active class from all tab links and content sections
+	for (var i = 0; i < tablinks.length; i++) {
+		tablinks[i].classList.remove('active');
 		sections[i].classList.remove('active');
 	}
 
-	// Show the selected content section
+	// Add the active class to the selected tab link and content section
+	document.getElementById(content + '-tab').classList.add('active');
 	document.getElementById(content + '-section').classList.add('active');
 }
